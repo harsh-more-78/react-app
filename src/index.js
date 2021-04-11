9b83a050310012c0108f08db75c858c859e7b684
harsh-more-78 <harshmore199@gmail.com>
2021-04-11 22:53:07 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+const fname = "Harsh"
+const lname = "More"
 ReactDOM.render(
-  <React.StrictMode>
-    <h1> Harsh More</h1>
-    <App />
-  </React.StrictMode>,
-  
+  <>
+    <h1>{`My name is ${fname}  ${lname}.`} </h1>
+    <p> Hello {} </p>
+  </>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
